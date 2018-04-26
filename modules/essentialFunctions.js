0970f5779de0dab028d96e2e8df6115c0423bfdd
Howesy <chazlikeskfc@gmail.com>
2018-04-26 19:36:45 +0100

    FINAL COMMIT: Finished my handler

--- modules/essentialFunctions.js
+++ modules/essentialFunctions.js
@@ -1,23 +1,23 @@
 module.exports = (bot) => {
-  bot.reload = command => {
-    return new Promise((resolve, reject) => {
-      try {
-        delete require.cache[require.resolve(`../commands/${command}`)];
-        let cmd = require(`../commands/${command}`);
-        bot.commands.delete(command);
-        bot.aliases.forEach((cmd, alias) => {
-          if (cmd === command) bot.aliases.delete(alias);
-        });
-        bot.commands.set(command, cmd);
-        cmd.conf.aliases.forEach(alias => {
-          bot.aliases.set(alias, cmd.help.name);
-        });
-        resolve();
-      } catch (e){
-        reject(e);
-      }
-    });
-  };
+    bot.reload = command => {
+      return new Promise((resolve, reject) => {
+        try {
+          delete require.cache[require.resolve(`../commands/${command}`)];
+          let cmd = require(`../commands/${command}`);
+          bot.commands.delete(command);
+          bot.aliases.forEach((cmd, alias) => {
+            if (cmd === command) bot.aliases.delete(alias);
+          });
+          bot.commands.set(command, cmd);
+          cmd.conf.aliases.forEach(alias => {
+            bot.aliases.set(alias, cmd.help.name);
+          });
+          resolve();
+        } catch (e){
+          reject(e);
+        }
+      });
+    };
 
     bot.checkMemberRole = async function(msg, memberID, roleName) {
       let roleBoolean = false;
