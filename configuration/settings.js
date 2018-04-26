93a0e14caf64e53d7b1f24af88f1897611bd6fe3
Howesy <chazlikeskfc@gmail.com>
2018-04-26 19:19:13 +0100

    [Updated] To match handler

--- configuration/settings.js
+++ configuration/settings.js
@@ -1,14 +1,14 @@
 const settings = {
   //These will be the require parts of my settings.
   "important" : {
-    "specifiedPrefix" : "Insert your prefix here!",
-    "specifiedOwner" : "Insert your Discord ID here!",
-    "specifiedToken" : "Insert your Discord client token here!",
-    "specifiedModRole" : "Insert your Mod role name here!"
+    "Prefix" : "Insert your prefix here!",
+    "Owner" : "Insert your Discord ID here!",
+    "Token" : "Insert your Discord client token here!",
+    "ModRole" : "Insert your Mod role name here!"
   },
-  //These will be unrequired but useful.
+  //These will be unrequired but useful in some cases.
   "config" : {
-    "specifiedHex": "Insert your hex colour here! In OxOOOOOO Format."
+    "Hex": "Insert your hex colour here! In OxOOOOOO Format."
   }
 };
 //Exporting my settings object as a module so I can append it to my client.
