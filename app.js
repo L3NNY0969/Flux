5eba0cd5f4d95290cbeae30e56e0378491d765a3
Howesy <chazlikeskfc@gmail.com>
2018-05-05 23:25:19 +0100

    [Update] Updated Event Parsing

--- app.js
+++ app.js
@@ -5,21 +5,31 @@ const FileSystem = require("fs");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.settings = require("./configuration/settings.js");
-require("./modules/eventParser.js")(bot);
 require("./modules/essentialFunctions.js")(bot);
 require("./modules/essentialEmbeds.js")(bot);
 bot.login(bot.settings.token);
 
 FileSystem.readdir("./commands/", function(error, commands) {
-    if (error) return console.error(error.message);
-    console.log(`Going to load: ${commands.length} commands!`);
-    commands.forEach(function(file) {
-        if (!file.endsWith(".js")) return;
-        let commandModule = require(`./commands/${file}`);
-        bot.commands.set(commandModule.help.name, commandModule);
-        console.log(`Command activated: ${commandModule.help.name}`);
-        commandModule.conf.aliases.forEach(function(alias) {
-            bot.aliases.set(alias, commandModule.help.name);
-        });
+  if (error) return console.error(error.stack);
+  console.log(`Going to load: ${commands.length} commands!`);
+  commands.forEach(function(file) {
+    if (!file.endsWith(".js")) return;
+    let commandModule = require(`./commands/${file}`);
+    bot.commands.set(commandModule.help.name, commandModule);
+    console.log(`Command activated: ${commandModule.help.name}`);
+    commandModule.conf.aliases.forEach(function(alias) {
+        bot.aliases.set(alias, commandModule.help.name);
     });
+  });
+});
+
+FileSystem.readdir("./events/", function(error, events) {
+  if (error) return console.error(error.stack);
+  console.log(`Going to load: ${events.length} events!`);
+  events.forEach(function(file) {
+    if (!file.endsWith(".js")) return;
+    let eventModule = require(`./events/${file}`);
+    console.log(`Loaded Event: ${eventModule.eventName}`);
+    bot.on(eventModule.eventName, eventModule.eventFunc);
+  });
 });
