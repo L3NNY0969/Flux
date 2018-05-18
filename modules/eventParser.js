6cacb31b6772f5f3d7b5fa444e0b8556bbcb9f7c
Howesy <chazlikeskfc@gmail.com>
2018-05-18 10:46:38 +0100

    Delete eventParser.js

--- modules/eventParser.js
+++
@@ -1,5 +0,0 @@
-const parseEvent = (event) => require(`../events/${event}`);
-module.exports = function(bot) {
-  bot.on('ready', () => parseEvent('ready')(bot));
-  bot.on('message', parseEvent('msg'));
-};
