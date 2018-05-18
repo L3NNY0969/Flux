5c6a010f801a01afee09cd117f87cb7e01208b37
Howesy <chazlikeskfc@gmail.com>
2018-05-18 12:34:02 +0100

    <Updated> Fixed to function with new parser

--- events/msg.js
+++ events/msg.js
@@ -1,26 +1,23 @@
-module.exports =  {
-  eventName: "message",
-  eventFunc: (msg) => {
-    let bot = msg.client;
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith(bot.settings.prefix)) return;
-    const command = msg.content.toLowerCase().split(" ")[0].slice(bot.settings.prefix.length);
-    const args = msg.content.split(" ").slice(1);
-    const permission = bot.permission(msg);
+exports.run = function(msg) {
+  let bot = msg.client;
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(bot.settings.prefix)) return;
+  const command = msg.content.toLowerCase().split(" ")[0].slice(bot.settings.prefix.length);
+  const args = msg.content.split(" ").slice(1);
+  const permission = bot.permission(msg);
     
-    let convertedCommand;
-    if (bot.commands.has(command)) {
-        convertedCommand = bot.commands.get(command);
-    } else if (bot.aliases.has(command)) {
-        convertedCommand = bot.commands.get(bot.aliases.get(command));
-    }
+  let convertedCommand;
+  if (bot.commands.has(command)) {
+      convertedCommand = bot.commands.get(command);
+  } else if (bot.aliases.has(command)) {
+      convertedCommand = bot.commands.get(bot.aliases.get(command));
+  }
   
-    if (convertedCommand && convertedCommand.conf.activated != true)
-      return msg.reply("I apologise but this command has been disabled by the bot creator");
+  if (convertedCommand && convertedCommand.conf.activated != true)
+    return msg.reply("I apologise but this command has been disabled by the bot creator");
   
-    if (convertedCommand) {
-      if (permission < convertedCommand.conf.permLevel) return;
-      convertedCommand.run(bot, msg, args, permission);
-    }
+  if (convertedCommand) {
+    if (permission < convertedCommand.conf.permLevel) return;
+    convertedCommand.run(bot, msg, args, permission);
   }
 };
