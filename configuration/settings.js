7d8e3faed429870e58e4bb35c7afc5931c95422d
Howesy <chazlikeskfc@gmail.com>
2018-05-23 00:23:59 +0100

    Delete settings.js

--- configuration/settings.js
+++
@@ -1,9 +0,0 @@
-//You can either put your hex in your embeds module, or here, your choice.
-const settings = {
-    "prefix" : "Desired prefix goes here!",
-    "owner" : "Desired owner ID goes here!",
-    "token" : "Desired bot token goes here!",
-    "modRole" : "Desired mod role name!",
-    "hex": "Desired hex colour here!"
-};
-module.exports = settings;
